0e9bfe15d19b2204269cf834a60980d0c1012711
gepron1x <georgijpronuk@gmail.com>
2021-12-25 17:00:30 +0300

    dumb license header

--- src/main/java/com/manya/key/KeyFactory.java
+++ src/main/java/com/manya/key/KeyFactory.java
@@ -1,10 +1,27 @@
+/*
+ * persistent-data-types
+ * Copyright © 2021 Lesya Morozova
+ *
+ * persistent-data-types is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * persistent-data-types is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with persistent-data-types. If not, see <https://www.gnu.org/licenses/>
+ * and navigate to version 3 of the GNU Lesser General Public License.
+ */
 package com.manya.key;
 
 import org.bukkit.NamespacedKey;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.function.Function;
 
 /**
  * an interface that represents factory of NamespacedKeys. I hate typing long constructor calls!
--- src/main/java/com/manya/pdc/base/BooleanDataType.java
+++ src/main/java/com/manya/pdc/base/BooleanDataType.java
@@ -1,7 +1,24 @@
+/*
+ * persistent-data-types
+ * Copyright © 2021 Lesya Morozova
+ *
+ * persistent-data-types is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * persistent-data-types is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with persistent-data-types. If not, see <https://www.gnu.org/licenses/>
+ * and navigate to version 3 of the GNU Lesser General Public License.
+ */
 package com.manya.pdc.base;
 
 import com.google.common.base.MoreObjects;
-import com.google.common.base.Optional;
 import org.bukkit.persistence.PersistentDataAdapterContext;
 import org.bukkit.persistence.PersistentDataType;
 import org.jetbrains.annotations.NotNull;
